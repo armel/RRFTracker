5f11ba5dc92931fbf4a1e56a6fa822a55a944368
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-04-06 06:39:16 +0200

    Try to fix strange QSO count bug

--- back/RRFTracker.py
+++ back/RRFTracker.py
@@ -105,12 +105,13 @@ def main(argv):
 
                 s.history = l.save_stat(s.history, s.call[1])
                 s.qso += 1
-            #else:
-            #    if s.tot_start is '':
-            #        s.tot_start = time.time()
-            #    s.tot_current = time.time()
-            #    if (s.blanc is True):         # Stat (same call but new PTT...)
-            #        s.history = l.save_stat(s.history, s.call[0])
+            else:
+                if s.tot_start is '':
+                    s.tot_start = time.time()
+                s.tot_current = time.time()
+                if (s.blanc is True):         # Stat (same call but new PTT...)
+                    s.history = l.save_stat(s.history, s.call[0])
+                    s.qso += 1
 
             s.blanc = False
 
