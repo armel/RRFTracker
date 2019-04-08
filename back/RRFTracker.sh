9202a51f5a044f489ecdc5ff36366a77883b5fdc
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-04-08 19:03:21 +0200

    Add shell script

--- back/RRFTracker.sh
+++ back/RRFTracker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /opt/RRFTracker_Web/
-nohup python /opt/RRFTracker_Web/back/RRFTracker.py --room RRF --log-path /var/www/mrtg/RRFTracker/ &
-nohup python /opt/RRFTracker_Web/back/RRFTracker.py --room TEC --log-path /var/www/mrtg/RRFTracker/ &
-#nohup python /opt/RRFTracker_Web/back/RRFTracker.py --room FON --log-path /var/www/mrtg/RRFTracker/ &
+nohup python /opt/RRFTracker_Web/back/RRFTracker.py --room RRF --log-path /var/www/mrtg/RRFTracker &
+nohup python /opt/RRFTracker_Web/back/RRFTracker.py --room TEC --log-path /var/www/mrtg/RRFTracker &
+#nohup python /opt/RRFTracker_Web/back/RRFTracker.py --room FON --log-path /var/www/mrtg/RRFTracker &
