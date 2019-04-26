c4cb3d3477fa3f0e27c7abb3027d526574fce134
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-04-26 14:54:31 +0200

    Add update shell script

--- back/RRFTracker_local.sh
+++ back/RRFTracker_local.sh
@@ -7,9 +7,9 @@ PATH_PID='/tmp'
 case "$1" in
     start)
         echo "Starting RRFTracker: RRF"
-        nohup python $PATH_SCRIPT --room RRF --log-path $PATH_LOG > /dev/null 2>&1 & echo $! > $PATH_PID/RRFTracker_RRF.pid
+        nohup python $PATH_SCRIPT --room RRF --log-path $PATH_LOG > $PATH_PID/RRFTracker_RRF.log 2>&1 & echo $! > $PATH_PID/RRFTracker_RRF.pid
         echo "Starting RRFTracker: TEC"
-        nohup python $PATH_SCRIPT --room TEC --log-path $PATH_LOG > /dev/null 2>&1 & echo $! > $PATH_PID/RRFTracker_TEC.pid
+        nohup python $PATH_SCRIPT --room TEC --log-path $PATH_LOG > $PATH_PID/RRFTracker_TEC.log 2>&1 & echo $! > $PATH_PID/RRFTracker_TEC.pid
         ;;
     stop) 
         echo "Stopping RRFTracker: RRF"
