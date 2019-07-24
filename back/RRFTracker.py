89b53fa7a5e4c6f2beb05e1750ddd5513ba069a3
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-07-24 22:37:22 +0200

    Refactoring room...

--- back/RRFTracker.py
+++ back/RRFTracker.py
@@ -33,8 +33,8 @@ def main(argv):
         elif opt in ('--log-path'):
             s.log_path = arg
         elif opt in ('--room'):
-            if arg not in ['RRF', 'RRF3', 'TECHNIQUE', 'INTERNATIONAL', 'BAVARDAGE', 'LOCAL', 'FON']:
-                print 'Unknown room name (choose between \'RRF\', \'RRF3\', \'TECHNIQUE\', \'INTERNATIONAL\', \'BAVARDAGE\', \'LOCAL\' and \'FON\')'
+            if arg not in ['RRF', 'RRF1', 'TECHNIQUE', 'INTERNATIONAL', 'BAVARDAGE', 'LOCAL', 'FON']:
+                print 'Unknown room name (choose between \'RRF\', \'RRF1\', \'TECHNIQUE\', \'INTERNATIONAL\', \'BAVARDAGE\', \'LOCAL\' and \'FON\')'
                 sys.exit()
             s.room = arg
 
