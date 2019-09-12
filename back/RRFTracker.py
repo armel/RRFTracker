2f47d3df4f0cdf3afdbf07ab5d63eb211fac908e
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2019-09-12 04:23:04 +0200

    Fix bug on history

--- back/RRFTracker.py
+++ back/RRFTracker.py
@@ -175,6 +175,10 @@ def main(argv):
 
             s.all = l.save_stat_all(s.all, s.call[0], tmp.strftime('%H:%M:%S'), l.convert_second_to_time(s.duration), False)
 
+            print s.all
+            sys.stdout.flush()
+
+
         # If no Transmitter...
         else:
             if s.transmit is True:
