028a505e0163220494bbc34527b0e3aa6f389c4c
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-23 23:07:31 +0100

    Change shell script

--- back/RRFTracker_prod.sh
+++ back/RRFTracker_prod.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 PATH_SCRIPT='/opt/RRFTracker/back/RRFTracker.py'
 PATH_MEDIA='/opt/RRFTracker/front/assets'
@@ -11,7 +11,20 @@ NOW=$(date +"%H%M%S")
 
 ROOMS=("RRF" "TECHNIQUE" "BAVARDAGE" "LOCAL" "INTERNATIONAL" "EXPERIMENTAL" "FON")
 
-for ROOM in "${ROOMS[@]}"
-do
-    echo $ROOM
-done
+case "$1" in
+    start)
+        for ROOM in "${ROOMS[@]}"
+        do
+            cp -a $PATH_MEDIA $PATH_LOG
+            echo "Init RRFTracker: $ROOM"
+            cp $PATH_HTML $PATH_LOG/RRF-today
+            #cp ${PATH_LOG}/${ROOM}-today/rrf.json ${PATH_LOG}/${ROOM}-today/rrf_${NOW}.json
+        done
+        echo "Starting RRFTracker"
+        nohup python3 $PATH_SCRIPT --log-path $PATH_LOG > $PATH_PID/RRFTracker.log 2>&1 & echo $! > $PATH_PID/RRFTracker.pid
+        ;;
+    stop) 
+        echo "Stopping RRFTracker"
+        kill `cat $PATH_PID/RRFTracker.pid`
+        ;;
+esac
