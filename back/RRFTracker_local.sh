f5eb43ba0cf5c6620816d296a5eafd3e734db93b
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-25 00:50:06 +0100

    Refactoring

--- back/RRFTracker_local.sh
+++ back/RRFTracker_local.sh
@@ -38,7 +38,6 @@ case "$1" in
         ;;
     stop) 
         echo "Stopping RRFTracker"
-        cp $PATH_PID/RRFTracker.log $PATH_PID/RRFTracker_${NOW)_${REP}.log
         kill `cat $PATH_PID/RRFTracker.pid`
         ;;
 esac
