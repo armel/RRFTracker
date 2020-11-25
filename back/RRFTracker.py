0a1f81db5b601b2d461a7cb2e84668dc091bd226
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-25 01:33:24 +0100

    Refactoring

--- back/RRFTracker.py
+++ back/RRFTracker.py
@@ -128,7 +128,7 @@ def main(argv):
                     s.init[s.room] = True       # Reset init
                 '''
 
-                if(s.now[:5] == '01:14'):
+                if(s.now[:5] == '01:35'):
                     s.log_path_day[s.room] = s.log_path + '/' + s.room + '-' + s.day + '-' + s.minute
 
                     if not os.path.exists(s.log_path_day[s.room]):
