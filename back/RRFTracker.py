75f37f1cc2019e3a4f57c01c784d33afa8a259f4
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-28 23:55:37 +0100

    Optimize

--- back/RRFTracker.py
+++ back/RRFTracker.py
@@ -17,8 +17,7 @@ import os
 import time
 import sys
 import getopt
-
-from retry_requests import retry
+import urllib3
 
 def main(argv):
 
@@ -109,48 +108,27 @@ def main(argv):
 
         # Request HTTP datas
 
-        session = retry(Session(), retries=5, backoff_factor=0.5)
-        session.get('https://foo.bar')
-
         try:
-            r = requests.get(s.room_list[s.room]['url'], verify=False, timeout=4)
-            data = r.json()
+            r = http.request('GET', s.room_list[s.room]['url'], timeout=0.5, retries=10)
+            data = json.loads(r.data.decode('utf-8'))
             transmitter = data['transmitter']
         except:
-            transmitter = None
-            print('Failed', s.day, s.now)
+            data = ''
 
-        # If transmitter...
-        if transmitter != None:
+        if data != '':
 
-            if s.transmit is False:
-                s.transmit = True
+            # If transmitter...
+            if transmitter != '':
 
-            s.call_current = l.sanitize_call(transmitter)
+                if s.transmit is False:
+                    s.transmit = True
 
-            if (s.call_previous != s.call_current):
-                s.tot_start = time.time()
-                s.tot_current = s.tot_start
-                s.call_previous = s.call_current
+                s.call_current = l.sanitize_call(transmitter)
 
-                if s.call_date[0] == '' or s.call_date[0] > s.now:
-                    blanc = 0
-                else:
-                    blanc = l.convert_time_to_second(s.now) - l.convert_time_to_second(s.call_date[0])
-
-                for i in range(9, 0, -1):
-                    s.call[i] = s.call[i - 1]
-                    s.call_date[i] = s.call_date[i - 1]
-                    s.call_blanc[i] = s.call_blanc[i - 1]
-                    s.call_time[i] = s.call_time[i - 1]
-
-                s.call[0] = s.call_current
-                s.call_blanc[0] = l.convert_second_to_time(blanc)
-
-            else:
-                if s.tot_start == '':
+                if (s.call_previous != s.call_current):
                     s.tot_start = time.time()
                     s.tot_current = s.tot_start
+                    s.call_previous = s.call_current
 
                     if s.call_date[0] == '' or s.call_date[0] > s.now:
                         blanc = 0
@@ -167,97 +145,118 @@ def main(argv):
                     s.call_blanc[0] = l.convert_second_to_time(blanc)
 
                 else:
-                    s.tot_current = time.time()
+                    if s.tot_start == '':
+                        s.tot_start = time.time()
+                        s.tot_current = s.tot_start
 
-            s.duration = int(s.tot_current) - int(s.tot_start)
+                        if s.call_date[0] == '' or s.call_date[0] > s.now:
+                            blanc = 0
+                        else:
+                            blanc = l.convert_time_to_second(s.now) - l.convert_time_to_second(s.call_date[0])
 
-            # Save stat only if real transmit
-            if (s.stat_save is False and s.duration > s.intempestif):
-                #s.node = l.save_stat_node(s.node, s.call[0], 0)
-                s.qso += 1
-                tmp = datetime.datetime.now() - datetime.timedelta(seconds=3)
+                        for i in range(9, 0, -1):
+                            s.call[i] = s.call[i - 1]
+                            s.call_date[i] = s.call_date[i - 1]
+                            s.call_blanc[i] = s.call_blanc[i - 1]
+                            s.call_time[i] = s.call_time[i - 1]
+
+                        s.call[0] = s.call_current
+                        s.call_blanc[0] = l.convert_second_to_time(blanc)
+
+                    else:
+                        s.tot_current = time.time()
+
+                s.duration = int(s.tot_current) - int(s.tot_start)
+
+                # Save stat only if real transmit
+                if (s.stat_save is False and s.duration > s.intempestif):
+                    #s.node = l.save_stat_node(s.node, s.call[0], 0)
+                    s.qso += 1
+                    tmp = datetime.datetime.now() - datetime.timedelta(seconds=3)
+                    s.qso_hour[s.hour] = s.qso - sum(s.qso_hour[:s.hour])
+                    s.all = l.save_stat_all(s.all, s.call[0], tmp.strftime('%H:%M:%S'), l.convert_second_to_time(s.duration), True)
+                    
+                    s.stat_save = True
+
+                # Format call time
+                tmp = datetime.datetime.now()
+                s.now = tmp.strftime('%H:%M:%S')
+                s.hour = int(tmp.strftime('%H'))
+
                 s.qso_hour[s.hour] = s.qso - sum(s.qso_hour[:s.hour])
-                s.all = l.save_stat_all(s.all, s.call[0], tmp.strftime('%H:%M:%S'), l.convert_second_to_time(s.duration), True)
-                
-                s.stat_save = True
 
-            # Format call time
-            tmp = datetime.datetime.now()
-            s.now = tmp.strftime('%H:%M:%S')
-            s.hour = int(tmp.strftime('%H'))
+                s.call_date[0] = s.now
+                s.call_time[0] = s.duration
 
-            s.qso_hour[s.hour] = s.qso - sum(s.qso_hour[:s.hour])
+                if s.duration > s.intempestif:
+                    s.all = l.save_stat_all(s.all, s.call[0], tmp.strftime('%H:%M:%S'), l.convert_second_to_time(s.duration), False)
 
-            s.call_date[0] = s.now
-            s.call_time[0] = s.duration
+            # If no Transmitter...
+            else:
+                if s.transmit is True:
+                    if s.room == 'RRF':
+                        if l.convert_second_to_time(s.duration) > s.tot_limit:
+                            tmp = datetime.datetime.now()
+                            s.tot = l.save_stat_tot(s.tot, s.call[0], tmp.strftime('%H:%M:%S'))
 
-            if s.duration > s.intempestif:
-                s.all = l.save_stat_all(s.all, s.call[0], tmp.strftime('%H:%M:%S'), l.convert_second_to_time(s.duration), False)
-
-        # If no Transmitter...
-        else:
-            if s.transmit is True:
-                if s.room == 'RRF':
-                    if l.convert_second_to_time(s.duration) > s.tot_limit:
+                    if s.stat_save is True:
+                        if s.duration > 600:    # I need to fix this bug...
+                            s.duration = 0
+                        s.day_duration += s.duration
+                    if s.stat_save is False:
                         tmp = datetime.datetime.now()
-                        s.tot = l.save_stat_tot(s.tot, s.call[0], tmp.strftime('%H:%M:%S'))
+                        s.porteuse = l.save_stat_porteuse(s.porteuse, s.call[0], tmp.strftime('%H:%M:%S'))
 
-                if s.stat_save is True:
-                    if s.duration > 600:    # I need to fix this bug...
-                        s.duration = 0
-                    s.day_duration += s.duration
-                if s.stat_save is False:
-                    tmp = datetime.datetime.now()
-                    s.porteuse = l.save_stat_porteuse(s.porteuse, s.call[0], tmp.strftime('%H:%M:%S'))
+                    s.transmit = False
+                    s.stat_save = False
+                    s.tot_current = ''
+                    s.tot_start = ''
 
-                s.transmit = False
-                s.stat_save = False
-                s.tot_current = ''
-                s.tot_start = ''
+            # Node analyze
 
-        # Node analyze
+            s.node_list=[]
+            for d in data['nodes']:
+                if d not in ['RRF', 'RRF2', 'RRF3', 'TECHNIQUE', 'BAVARDAGE', 'INTERNATIONAL', 'LOCAL', 'EXPERIMENTAL']:
+                    s.node_list.append(l.sanitize_call(d))    
 
-        s.node_list=[]
-        for d in data['nodes']:
-            if d not in ['RRF', 'RRF2', 'RRF3', 'TECHNIQUE', 'BAVARDAGE', 'INTERNATIONAL', 'LOCAL', 'EXPERIMENTAL']:
-                s.node_list.append(l.sanitize_call(d))    
-
-        if s.node_list_old == []:
-            s.node_list_old = s.node_list
-        else:
-            if s.node_list_old != s.node_list:
-                if (list(set(s.node_list_old) - set(s.node_list))):
-                    s.node_list_out = list(set(s.node_list_old) - set(s.node_list))
-                    for n in s.node_list_out:
-                        if n in s.node_list_in:
-                            s.node_list_in.remove(n)
-                    s.node_list_out = sorted(s.node_list_out)
-
-                if (list(set(s.node_list) - set(s.node_list_old))):
-                    s.node_list_in = list(set(s.node_list) - set(s.node_list_old))
-                    for n in s.node_list_in:
-                        if n in s.node_list_out:
-                            s.node_list_out.remove(n)
-                    s.node_list_in = sorted(s.node_list_in)
-
+            if s.node_list_old == []:
                 s.node_list_old = s.node_list
+            else:
+                if s.node_list_old != s.node_list:
+                    if (list(set(s.node_list_old) - set(s.node_list))):
+                        s.node_list_out = list(set(s.node_list_old) - set(s.node_list))
+                        for n in s.node_list_out:
+                            if n in s.node_list_in:
+                                s.node_list_in.remove(n)
+                        s.node_list_out = sorted(s.node_list_out)
 
-        s.node_count = len(s.node_list)
+                    if (list(set(s.node_list) - set(s.node_list_old))):
+                        s.node_list_in = list(set(s.node_list) - set(s.node_list_old))
+                        for n in s.node_list_in:
+                            if n in s.node_list_out:
+                                s.node_list_out.remove(n)
+                        s.node_list_in = sorted(s.node_list_in)
 
-        if s.node_count > s.node_count_max:
-            s.node_count_max = s.node_count
+                    s.node_list_old = s.node_list
 
-        if s.node_count < s.node_count_min:
-            s.node_count_min = s.node_count
+            s.node_count = len(s.node_list)
 
-        # Compute duration
-        if s.transmit is True and s.tot_current > s.tot_start:
-            s.duration = int(s.tot_current) - int(s.tot_start)
-        if s.transmit is False:
-            s.duration = 0
+            if s.node_count > s.node_count_max:
+                s.node_count_max = s.node_count
 
-        # Save log
-        l.log_write()
+            if s.node_count < s.node_count_min:
+                s.node_count_min = s.node_count
+
+            # Compute duration
+            if s.transmit is True and s.tot_current > s.tot_start:
+                s.duration = int(s.tot_current) - int(s.tot_start)
+            if s.transmit is False:
+                s.duration = 0
+
+            # Save log
+            l.log_write()
+
+        # Manage tempo if necessary
 
         chrono_stop = time.time()
         chrono_time = chrono_stop - chrono_start
