53fa1be202001523971a6eaa5e269c11dd74dc2e
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-28 08:40:34 +0100

    Tune tempo

--- back/RRFTracker.py
+++ back/RRFTracker.py
@@ -107,7 +107,7 @@ def main(argv):
 
         # Request HTTP datas
         try:
-            r = requests.get(s.room_list[s.room]['url'], verify=False, timeout=.75)
+            r = requests.get(s.room_list[s.room]['url'], verify=False, timeout=2)
             page = r.content.decode('utf-8')
 
             search_start = page.find('transmitter":"')            # Search this pattern
